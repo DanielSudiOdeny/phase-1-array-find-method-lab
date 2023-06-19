7f0af528438f4d338b081793dfc21deca9fc4cd7
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-06-19 21:04:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+  let winningRecord;
+  record.find((record) => {
+    if (record["result"] === "W") {
+      winningRecord = record.year;
+      return winningRecord;
+    }
+  });
+  return winningRecord;
+}
+
+console.log(superbowlWin(record));
